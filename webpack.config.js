2f674c035d716946e837f9a75a07db852621978d
o2ang <29375210+o2ang@users.noreply.github.com>
2024-09-20 09:36:03 +0900

    Load Images with file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { __esModule } = require("mini-css-extract-plugin/dist");
 
 module.exports = {
   entry: "./src/javascripts/main.js",
@@ -19,6 +20,18 @@ module.exports = {
           },
           {
             loader: "css-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg)/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name: "images/[name].[ext]",
+            },
           },
         ],
       },
